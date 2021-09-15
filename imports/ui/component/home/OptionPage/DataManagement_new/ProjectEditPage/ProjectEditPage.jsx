848a2fa6b757cd578f39d79a85d9e1210c93b5cf
coco-peter <dklirser@gmail.com>
2021-09-15 14:10:53 +0900

    generate new option page

---
+++ imports/ui/component/home/OptionPage/DataManagement_new/ProjectEditPage/ProjectEditPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import styles from "./ProjectEditPage.module.css";
+
+export default function ProjectEditPage() {
+  return (
+    <div className={styles.pageWrap}>
+      <div></div>
+    </div>
+  );
+}

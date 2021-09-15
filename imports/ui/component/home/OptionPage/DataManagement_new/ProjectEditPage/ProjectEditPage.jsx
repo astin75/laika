2448111cb41b272aa642ca303803bf743eae15c6
coco-peter <dklirser@gmail.com>
2021-09-15 18:58:26 +0900

    optionPage :html ,css

--- imports/ui/component/home/OptionPage/DataManagement_new/ProjectEditPage/ProjectEditPage.jsx
+++ imports/ui/component/home/OptionPage/DataManagement_new/ProjectEditPage/ProjectEditPage.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 
 import styles from "./ProjectEditPage.module.css";
+import ProjectDetailPage from "./ProjectDetailPage/ProjectDetailPage";
 
 export default function ProjectEditPage() {
   return (
     <div className={styles.pageWrap}>
-      <div></div>
+      <ProjectDetailPage />
+      <div className={styles.generateProjectBtn}>Register Project</div>
+      <div className={styles.generateStateValue}></div>
     </div>
   );
 }

1eeb9c532965439da15a766f68e645a62b8a4dac
명혜은 <hayluna2@gmail.com>
2021-09-18 21:44:16 +0900

    fix: file import fix

--- client/main.jsx
+++ client/main.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import { Meteor } from 'meteor/meteor'
 import { render } from 'react-dom'
-import { App } from '../imports/ui/App'
+import App from 'imports/ui/App'
 import '@fortawesome/fontawesome-free/js/all.js'
+import { Router } from 'react-router'
+import { createBrowserHistory } from 'history'
 
 Meteor.startup(() => {
-  render(<App />, document.getElementById('app'))
+  const history = createBrowserHistory()
+
+  render(
+    <Router history={history}>
+      <App />
+    </Router>,
+    document.getElementById('app')
+  )
 })
--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,48 +1,55 @@
 import React from 'react'
 import { NotificationsProvider } from '@mantine/notifications'
 import './styles/style.scss'
-import { Router } from 'react-router'
-import { createBrowserHistory } from 'history'
+import { Switch, Route } from 'react-router-dom'
+import DefaultLayout from 'imports/ui/layout/DefaultLayout'
+import Home from 'imports/ui/component/home/Index'
+import LabelingPage from 'imports/ui/component/home/OptionPage/LabelingPage/LabelingPage'
+import { OptionPage } from 'imports/ui/component/home/OptionPage/DataManagement/OptionPage'
+// import OptionPage from "imports/ui/component/home/OptionPage/DataManagement_new/OptionPage";
+import AccountPage from 'imports/ui/component/home/AccountPage/AccountPage'
+import ProjectList2 from 'imports/ui/component/home/OptionPage/DataManagement/projectList2/ProjectList2'
+import ProjectUpload from 'imports/ui/component/home/OptionPage/DataManagement/projectUpload/ProjectUpload'
 
-export const App = () => {
-  const history = createBrowserHistory()
+import NotFound from 'imports/ui/component/home/NotFound'
 
+const App = () => {
   return (
     <NotificationsProvider>
-      <Router history={history}>
-        <Switch>
-          <Route
-            exact
-            path={[
-              '/',
-              '/labelingPage',
-              '/optionPage',
-              '/accountPage',
-              '/projectListPage',
-              '/projectManagementPage',
-            ]}
-          >
-            <DefaultLayout>
-              <Switch>
-                <Route exact path="/" component={Home} />
-                <Route exact path="/labelingPage" component={LabelingPage} />
-                <Route exact path="/optionPage" component={OptionPage} />
-                <Route exact path="/accountPage" component={AccountPage} />
-                <Route exact path="/projectListPage" component={ProjectList2} />
-                <Route exact path="/projectManagementPage" component={ProjectUpload} />
-              </Switch>
-            </DefaultLayout>
-          </Route>
+      <Switch>
+        <Route
+          exact
+          path={[
+            '/',
+            '/labelingPage',
+            '/optionPage',
+            '/accountPage',
+            '/projectListPage',
+            '/projectManagementPage',
+          ]}
+        >
+          <DefaultLayout>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/labelingPage" component={LabelingPage} />
+              <Route exact path="/optionPage" component={OptionPage} />
+              <Route exact path="/accountPage" component={AccountPage} />
+              <Route exact path="/projectListPage" component={ProjectList2} />
+              <Route exact path="/projectManagementPage" component={ProjectUpload} />
+            </Switch>
+          </DefaultLayout>
+        </Route>
 
-          {/***********
+        {/***********
             404
         ***********/}
 
-          <Route path="*" component={NotFound} />
-        </Switch>
+        <Route path="*" component={NotFound} />
+      </Switch>
 
-        <Route />
-      </Router>
+      <Route />
     </NotificationsProvider>
   )
 }
+
+export default App

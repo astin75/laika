b4a07bae7538a72509ff3e5e373a28636e07be49
명혜은 <hayluna2@gmail.com>
2021-09-18 21:00:24 +0900

    chore: eslint plugin추가

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,14 +1,48 @@
 import React from 'react'
 import { NotificationsProvider } from '@mantine/notifications'
-import Route from './route'
-import './assets/sass/style.scss'
+import './styles/style.scss'
+import { Router } from 'react-router'
+import { createBrowserHistory } from 'history'
 
 export const App = () => {
+  const history = createBrowserHistory()
+
   return (
-    <>
-      <NotificationsProvider>
+    <NotificationsProvider>
+      <Router history={history}>
+        <Switch>
+          <Route
+            exact
+            path={[
+              '/',
+              '/labelingPage',
+              '/optionPage',
+              '/accountPage',
+              '/projectListPage',
+              '/projectManagementPage',
+            ]}
+          >
+            <DefaultLayout>
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/labelingPage" component={LabelingPage} />
+                <Route exact path="/optionPage" component={OptionPage} />
+                <Route exact path="/accountPage" component={AccountPage} />
+                <Route exact path="/projectListPage" component={ProjectList2} />
+                <Route exact path="/projectManagementPage" component={ProjectUpload} />
+              </Switch>
+            </DefaultLayout>
+          </Route>
+
+          {/***********
+            404
+        ***********/}
+
+          <Route path="*" component={NotFound} />
+        </Switch>
+
         <Route />
-      </NotificationsProvider>
-    </>
+      </Router>
+    </NotificationsProvider>
   )
 }

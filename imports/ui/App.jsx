fb6543c7226bf2439d75e33f0125b0a3938e1f00
coco-peter <dklirser@gmail.com>
2021-09-22 07:31:54 +0900

    change directory url

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -13,8 +13,8 @@ import IndexPage from 'ui/pages/IndexPage';
 import LabelingPage from 'ui/pages/labelPage/LabelingPage';
 import NotFound from 'ui/pages/NotFound';
 import { OptionPage } from 'ui/pages/OptionPage/DataManagement/OptionPage';
-import ProjectList2 from 'ui/pages/OptionPage/DataManagement/ProjectList2/ProjectList2';
-import ProjectUpload from 'ui/pages/OptionPage/DataManagement/ProjectUpload/ProjectUpload';
+import ProjectList2 from 'ui/pages/OptionPage/DataManagement/projectList2/ProjectList2';
+import ProjectUpload from 'ui/pages/OptionPage/DataManagement/projectUpload/ProjectUpload';
 import UserControl from 'ui/pages/OptionPage/DataManagement/UserControl/UserControl';
 import RoadMap from 'ui/pages/RoadMap/RoadMap';
 

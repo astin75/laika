9fc7f5fd56e679935f3bfa4e33399969e4ff7e43
astin75 <astin75@gmail.com>
2021-09-22 23:53:46 +0900

    file download working..

--- imports/ui/pages/RoadMap/RoadMap.jsx
+++ imports/ui/pages/RoadMap/RoadMap.jsx
@@ -1,56 +1,119 @@
-import { Button, Select, Table, Text, Timeline } from '@mantine/core'
+import { Button, Select, Table, Text, Timeline } from '@mantine/core';
+import saveAs from 'file-saver';
 //-----------------------------------------------
-import { imageInfoCollection, userProfileCollection } from 'imports/db/collections'
-import { gtInfoCollection } from 'imports/db/collections'
-import { projectCollection } from 'imports/db/collections'
-import { Meteor } from 'meteor/meteor'
-import { useTracker } from 'meteor/react-meteor-data'
-import React, { createRef, useRef, useState } from 'react'
+import { imageInfoCollection, userProfileCollection } from 'imports/db/collections';
+import { gtInfoCollection } from 'imports/db/collections';
+import { projectCollection } from 'imports/db/collections';
+import Images from 'imports/db/files';
+import JSZip from 'jszip';
+import JSZipUtils from 'jszip-utils';
+import { Meteor } from 'meteor/meteor';
+import { useTracker } from 'meteor/react-meteor-data';
+import React, { createRef, useRef, useState } from 'react';
 //----------------------------------------------
-import NavigationBar from 'ui/components/NavigationBar/NavigationBar'
-import styles from 'ui/pages/OptionPage/DataManagement/UserControl/UserControl.module.css'
+import NavigationBar from 'ui/components/NavigationBar/NavigationBar';
+import styles from 'ui/pages/OptionPage/DataManagement/UserControl/UserControl.module.css';
 
-import styels from './RoadMap.module.css'
+import styels from './RoadMap.module.css';
 
 export default function RoadMap() {
-  const [downloadUrl, setDownloadUrl] = useState('')
-  const downClick = useRef(null)
+  this.state = {
+    fileType: 'json',
+    fileDownloadUrl: null,
+    status: '',
+    data: [
+      { state: 'Arizona', electors: 11 },
+      { state: 'Florida', electors: 29 },
+      { state: 'Iowa', electors: 6 },
+      { state: 'Michigan', electors: 16 },
+      { state: 'North Carolina', electors: 15 },
+      { state: 'Ohio', electors: 18 },
+      { state: 'Pennsylvania', electors: 20 },
+      { state: 'Wisconsin', electors: 10 },
+    ],
+  };
+  const downloadFile = ({ data, fileName, fileType }) => {
+    // Create a blob with the data we want to download as a file
+    const blob = new Blob([data], { type: fileType });
+    // Create an anchor element and dispatch a click event on it
+    // to trigger a download
+    const a = document.createElement('a');
+    a.download = fileName;
+    a.href = window.URL.createObjectURL(blob);
+    const clickEvt = new MouseEvent('click', {
+      view: window,
+      bubbles: true,
+      cancelable: true,
+    });
+    a.dispatchEvent(clickEvt);
+    a.remove();
+  };
 
-  const projectList = useTracker(() => projectCollection.find({}).fetch())
+  const multiDownloadFile = (links) => {
+    let zip = new JSZip();
+    var count = 0;
+    let zipFilename = 'Pictures.zip';
+
+    links.forEach(function (url, i) {
+      var filename = i + '.json';
+
+      let blob = new Blob([JSON.stringify(url, null, 4)], { type: 'text/json' });
+      let webUrl = URL.createObjectURL(blob);
+
+      // console.log(url, i);
+      //
+      // // loading a file and add it in a zip file
+      // // JSZipUtils.getBinaryContent(webUrl, function (err, data) {
+      // //   if (err) {
+      // //     throw err; // or handle the error
+      // //   }
+      //
+      // zip.file(filename, blob, { binary: true });
+      //
+      // count++;
+      // if (count == links.length) {
+      //   zip.generateAsync({ type: 'blob' }).then(function (content) {
+      //     saveAs(content, zipFilename);
+      //   });
+      // }
+    });
+  };
+
+  const [downloadUrl, setDownloadUrl] = useState('');
+  const downClick = useRef(null);
+
+  const projectList = useTracker(() => projectCollection.find({}).fetch());
   const onRankChange = (e, projectName) => {
-    let gtinfo = gtInfoCollection.find({ projectName: projectName }).fetch()
-    let imageInfo = imageInfoCollection.find({ projectName: projectName }).fetch()
-  }
+    let gtinfo = gtInfoCollection.find({ projectName: projectName }).fetch();
+    let imageInfo = imageInfoCollection.find({ projectName: projectName }).fetch();
+  };
 
   // console.log(gtinfo, 1)
   // console.log(imageInfo, 2)
 
   //userProfileCollection.update(oops._id, { $set: { rank: rank } })
 
-  const onDownload = (projectName) => {
-    let gtinfo = gtInfoCollection.find({ projectName: projectName }).fetch()
-    let imageInfo = imageInfoCollection.find({ projectName: projectName }).fetch()
-    e.preventDefault()
-    let output
-    let fileType = 'json'
-    if (fileType === 'json') {
-      output = JSON.stringify({ states: this.state.data }, null, 4)
-      const blob = new Blob([output])
-      setDownloadUrl(URL.createObjectURL(blob))
-      downClick.current.URL.revokeObjectURL(downloadUrl)
-
-      setDownloadUrl(URL.createObjectURL(blob))
-    }
-  }
+  const onDownload = (e, projectName) => {
+    let gtinfo = gtInfoCollection.find({ projectName: projectName }).fetch();
+    let imgRaw = Images.find({ public: projectName }).cursor;
+    console.log(imgRaw);
+    e.preventDefault();
+    multiDownloadFile(gtinfo);
+    // downloadFile({
+    //   data: JSON.stringify(gtinfo[0], null, 4),
+    //   fileName: 'users.json',
+    //   fileType: 'text/json',
+    // });
+  };
 
   const onDelete = (userName) => {
-    let oops = Meteor.users.findOne({ userName: userName })
-    Meteor.users.remove(oops._id)
-  }
+    let oops = Meteor.users.findOne({ userName: userName });
+    Meteor.users.remove(oops._id);
+  };
 
   const handleChange = (option) => {
-    setRank(option)
-  }
+    setRank(option);
+  };
 
   return (
     <div className={styels.container}>
@@ -87,7 +150,12 @@ export default function RoadMap() {
                   >
                     삭제
                   </Button>
-                  <Button size="compact-sm" onClick={(event) => onDownload(event, x.projectName)}>
+                  <Button
+                    download={'states.json'}
+                    href={downloadUrl}
+                    size="compact-sm"
+                    onClick={(event) => onDownload(event, x.projectName)}
+                  >
                     다운로드
                   </Button>
                 </td>
@@ -95,9 +163,7 @@ export default function RoadMap() {
             ))}
         </tbody>
       </Table>
-      <a className="hidden" download={'states.json'} href={downloadUrl} ref={downClick}>
-        download it
-      </a>
+
       {/*<Timeline active={1} bulletSize={24} lineWidth={3}>*/}
       {/*  <Timeline.Item title="New branch">*/}
       {/*    <Text color="white" size="md">*/}
@@ -153,5 +219,5 @@ export default function RoadMap() {
       {/*</Timeline>*/}
       <NavigationBar />
     </div>
-  )
+  );
 }

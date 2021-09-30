45f0ebf0b6f8f4c62c1d7e249342a4f609e9d001
astin75 <astin75@gmail.com>
2021-09-30 15:59:09 +0900

    색추가

--- imports/ui/pages/OptionPage/DataManagement/ProjectUpload/ProjectUpload.jsx
+++ imports/ui/pages/OptionPage/DataManagement/ProjectUpload/ProjectUpload.jsx
@@ -128,20 +128,20 @@ export default function ProjectUpload() {
       gtInfoCollection.insert(tempGroundTruthJson[imageIndex]);
     }
     let colorHex = [
-      '#25262b',
-      '#868e96',
+      '#12b886',
       '#fa5252',
+      '#fd7e14',
+      '#fab005',
+      '#7950f2',
       '#e64980',
       '#be4bdb',
-      '#7950f2',
       '#4c6ef5',
       '#228be6',
       '#15aabf',
-      '#12b886',
       '#40c057',
       '#82c91e',
-      '#fab005',
-      '#fd7e14',
+
+
     ];
     let tempProjectInfo = {
       projectName: projectName[0].projectName,

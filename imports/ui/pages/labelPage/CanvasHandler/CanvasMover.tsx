974858f5218009c648d8ed2bd42359114b4c5a48
hanhyuk <nalnez13@gmail.com>
2021-10-02 13:33:48 +0900

    캔버스에서 객체 선택

--- imports/ui/pages/labelPage/CanvasHandler/CanvasMover.tsx
+++ imports/ui/pages/labelPage/CanvasHandler/CanvasMover.tsx
@@ -1,26 +1,41 @@
 import React, { useState } from 'react';
-import { useRecoilValue } from 'recoil';
+import { useRecoilState, useRecoilValue } from 'recoil';
 import { IPoint } from '../../../../canvasTools/IPoint';
 import {
   canvasView,
-  canvasViewDispatcherState,
+  canvasViewDispatcherState
 } from '../../../../recoil/canvas';
 import Canvas from '../Canvas';
 import { ICanvasHandlerProps } from './ICanvasHandler';
+import { annotationDispatcherState, currentAnnotations, selectionIdx } from '../../../../recoil/annotation';
+import { isPointInRect } from '../../../../canvasTools/IRect';
+import { findNearestRectRegion } from '../../../../canvasTools/IRegionData';
 
-type HandlerState = 'idle' | 'moveCanvas';
+type HandlerState = 'idle' | 'moveCanvas' | 'onRegion';
 
 export default function CanvasMover({ frame, onWheel }: ICanvasHandlerProps) {
   const [state, setState] = useState<HandlerState>('idle');
-  const view = useRecoilValue(canvasView);
   const canvasViewDispatcher = useRecoilValue(canvasViewDispatcherState);
+  const annotations = useRecoilValue(currentAnnotations);
+  const [selection, setSelection] = useRecoilState(selectionIdx);
+  const annotationDispatcher = useRecoilValue(annotationDispatcherState);
+  const view = useRecoilValue(canvasView);
 
   const handleMouseDown = (e: React.MouseEvent) => {
     if (e.button !== 0) return;
+    const mousePoint: IPoint = {
+      x: e.nativeEvent.offsetX,
+      y: e.nativeEvent.offsetY
+    };
     switch (state) {
       case 'idle':
         setState('moveCanvas');
         break;
+      case 'onRegion': {
+        const nearestRect = findNearestRectRegion(mousePoint, annotations, view);
+        setSelection(nearestRect);
+        break;
+      }
       default:
         break;
     }
@@ -29,10 +44,24 @@ export default function CanvasMover({ frame, onWheel }: ICanvasHandlerProps) {
   const handleMouseMove = (e: React.MouseEvent) => {
     const movementOffset: IPoint = {
       x: e.movementX,
-      y: e.movementY,
+      y: e.movementY
+    };
+    const mousePoint: IPoint = {
+      x: e.nativeEvent.offsetX,
+      y: e.nativeEvent.offsetY
     };
 
     switch (state) {
+      case 'idle':
+      case 'onRegion': {
+        const nearestRect = findNearestRectRegion(mousePoint, annotations, view);
+        if (nearestRect !== undefined)
+          setState('onRegion');
+        else
+          setState('idle');
+        annotationDispatcher?.highlightRect(nearestRect, undefined);
+      }
+        break;
       case 'moveCanvas':
         canvasViewDispatcher?.shiftCanvas(movementOffset);
         break;

738c68ea29900579ca34d238b5e0886f690e0e2d
HanHyuk <nalnez13@gmail.com>
2021-10-06 21:44:15 +0900

    클래스 선택 버그 수정

--- imports/ui/pages/labelPage/ObjectPage/CuurentObject/CurrentObjectInfor/state/StateInfor.jsx
+++ imports/ui/pages/labelPage/ObjectPage/CuurentObject/CurrentObjectInfor/state/StateInfor.jsx
@@ -1,10 +1,14 @@
+import { ColorPicker, Select } from '@mantine/core';
+import {
+  annotationDispatcherState,
+  currentAnnotations,
+  selectionIdx,
+} from 'imports/recoil/annotation';
+import _ from 'lodash';
 import React, { useEffect, useState } from 'react';
+import { useRecoilValue } from 'recoil';
 
 import styles from './StateInfor.module.css';
-import { Select, ColorPicker } from '@mantine/core';
-import { useRecoilValue } from 'recoil';
-import { annotationDispatcherState, currentAnnotations, selectionIdx } from 'imports/recoil/annotation';
-import _ from 'lodash';
 
 export default function StateInfor({ state, enableKey }) {
   const annotations = useRecoilValue(currentAnnotations);
@@ -15,10 +19,8 @@ export default function StateInfor({ state, enableKey }) {
 
   const updateAnnotationState = (e) => {
     const newAnnot = _.cloneDeep(annotations[selection]);
-    if (e === '선택안됨')
-      delete newAnnot.meta[state.stateName];
-    else
-      newAnnot.meta[state.stateName] = e;
+    if (e === '선택안됨') delete newAnnot.meta[state.stateName];
+    else newAnnot.meta[state.stateName] = e;
     annotationDispatcher?.edit(selection, newAnnot, false);
   };
 
@@ -26,11 +28,12 @@ export default function StateInfor({ state, enableKey }) {
     const newAnnot = _.cloneDeep(annotations[selection]);
     if (e.key === '[') {
       newAnnot.meta[state.stateName] = stateClassInfor[0];
+      annotationDispatcher?.edit(selection, newAnnot, false);
     }
     if (e.key === ']') {
       newAnnot.meta[state.stateName] = stateClassInfor[1];
+      annotationDispatcher?.edit(selection, newAnnot, false);
     }
-    annotationDispatcher?.edit(selection, newAnnot, false);
   };
   useEffect(() => {
     if (enableKey) {
@@ -45,10 +48,15 @@ export default function StateInfor({ state, enableKey }) {
     <div className={styles.stateInforWrap}>
       <div>{state.stateName}</div>
 
-      <Select size='xs' data={stateClassInfor} value={annotations[selection]?.meta[state.stateName] ?? '선택안됨'}
-              onChange={(e) => {
-                updateAnnotationState(e);
-              }} style={{ width: '80px' }} />
+      <Select
+        size="xs"
+        data={stateClassInfor}
+        value={annotations[selection]?.meta[state.stateName] ?? '선택안됨'}
+        onChange={(e) => {
+          updateAnnotationState(e);
+        }}
+        style={{ width: '80px' }}
+      />
     </div>
   );
 }
